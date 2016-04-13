fc1536763a63ec0f579d8e815795eb6732a34032
Pavel <pavel@dahncke.net>
2016-04-13 23:47:33 +0200

    new subs
    slack_define | slack_undef | slack_set

--- modules/98_slack.pm
+++ modules/98_slack.pm
@@ -17,3 +17,41 @@ sub slack_Initialize($) {
           "formal:yes,no "
         . $readingFnAttributes;
 }
+
+
+sub slack_Define($$) {
+    my ($hash, $def) = @_;
+    my @param = split('[ \t]+', $def);
+    
+    if(int(@param) > 2) {
+        return "too many parameters: define <name> slack";
+    }
+    
+    my $hash->{name}  = $param[0];
+    
+    return undef;
+}
+
+sub slack_Undef($$) {
+    my ($hash, $arg) = @_; 
+    # nothing to do
+    return undef;
+}
+
+sub slack_Set($@) {
+    my ($hash, @param) = @_;
+    
+    return '"set slack" needs at least one argument' if (int(@param) < 2);
+    
+    my $name = shift @param;
+    my $opt = shift @param;
+    my $value = join("", @param);
+    
+    if(!defined($Hello_gets{$opt})) {
+        my @cList = keys %Hello_gets;
+        return "Unknown argument $opt, choose one of " . join(" ", @cList);
+    }
+    $hash->{STATE} = $Hello_gets{$opt} = $value;
+    
+    return "$opt set to $value. Try to get it.";
+}

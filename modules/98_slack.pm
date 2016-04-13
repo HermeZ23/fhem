8cada52e7a349db46f25498e140308d4e79beeb5
Pavel <pavel@dahncke.net>
2016-04-14 00:01:03 +0200

    add html to slack module

--- modules/98_slack.pm
+++ modules/98_slack.pm
@@ -55,3 +55,52 @@ sub slack_Set($@) {
     
     return "$opt set to $value. Try to get it.";
 }
+
+
+1;
+
+=pod
+=begin html
+
+<a name="slack"></a>
+<h3>slack</h3>
+<ul>
+    <i>slack</i> implements notification to slack / maybe more in the future
+    <br><br>
+    <a name="slackdefine"></a>
+    <b>Define</b>
+    <ul>
+        <code>define <name> Hello </code>
+        <br><br>
+        Example: <code>define testslack slack </code>
+        <br><br>
+    </ul>
+    <br>
+    
+    <a name="slackset"></a>
+    <b>Set</b><br>
+    <ul>
+        <code>set <name> <option> <value></code>
+        <br><br>
+        You can <i>set</i> any value to any of the following options. They're just there to 
+        <i>get</i> them. See <a href="http://fhem.de/commandref.html#set">commandref#set</a> 
+        for more info about the set command.
+        <br><br>
+        Options:
+        <ul>
+              <li><i>satisfaction</i><br>
+                  Defaults to "no"</li>
+              <li><i>whatyouwant</i><br>
+                  Defaults to "can't"</li>
+              <li><i>whatyouneed</i><br>
+                  Defaults to "try sometimes"</li>
+        </ul>
+    </ul>
+    <br>
+
+
+</ul>
+
+=end html
+
+=cut
